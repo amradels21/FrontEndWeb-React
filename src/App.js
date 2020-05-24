0f0faad00f02a56a7aa945b7bdeafdb09f24f484
Amr Adel <amradel21@icloud.com>
2020-05-24 20:35:32 +0200

    cardtype list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import './App.css';
 import Menu from './components/MenuComponent';
+import './App.css';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state =  {
+      dishes: DISHES
+    }
+  }
+
+  render(){
+    return (
     <div>
       <Navbar dark color="primary">
           <div className="container">
@@ -13,10 +23,12 @@ function App() {
           </div>
         </Navbar>
 
-        <Menu />
+        <Menu dishes = {this.state.dishes} />
 
     </div>
   );
+}
+  
 }
 
 export default App;
