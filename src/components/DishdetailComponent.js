954d74b2cc5ccbf363d519fc141889c1248685cc
Amr Adel <amradel21@icloud.com>
2020-06-02 00:35:29 +0200

    MainComponent

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody,
+    CardTitle } from 'reactstrap';
+
+class DishDetail extends Component
+    {
+    constructor(props){
+         super(props);
+        }
+
+    renderDish(dish){
+        if(dish!=null)
+            return(
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                      <CardTitle>{dish.name}</CardTitle>
+                      <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+         );
+
+         else
+            return(
+            <div></div>
+            );
+        }
+
+
+    renderComments(comments) {
+        if(comments!=null){
+              
+            const commentDetails = comments.map((comment)=> {
+                   
+                const options = {
+                  year: 'numeric',
+                  month: 'short',
+                   day: '2-digit'
+                       }
+                   
+                return(
+                    <li key={comment.id}>
+                        <p>{comment.comment}</p>
+                        <p>-- {comment.author},
+                        &nbsp;
+                        {new Intl.DateTimeFormat('en-US',options).format(new Date(comment.date))}
+                        </p>
+                     </li>
+                    );
+             });
+            
+            return(
+                <div>
+                    <h4>Comments</h4>
+                    <ul className="list-unstyled">
+                        {commentDetails}
+                    </ul>
+                </div>
+            );
+           }
+
+           else
+           {
+               return(
+               <div></div>
+               );
+           }
+           
+        }
+
+
+
+     render(){
+            
+        const dish = this.props.dish
+        if (dish == null) {
+                return (
+                <div></div>
+                );
+            }
+            
+           
+        return(
+            <div className="container">
+                <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            {this.renderDish(dish)}
+                        </div>
+
+                        <div className="col-12 col-md-5 m-1">
+                             {this.renderComments(dish.comments)}
+                        </div>
+                </div>
+            </div>
+                
+            );
+        }
+    }
+
+export default DishDetail;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -4,14 +4,18 @@ import { Media, Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } f
 class Menu extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            selectedDish: null
-        }
+    
     }
 
-    onDishSelect(dish) {
-        this.setState({selectedDish: dish});
+    componentDidMount(){
+        console.log('Menu Component componentDidMount is invoked');
+
     }
+
+
+ 
+
+    
 
     renderDish(dish){
         if(dish != null){
@@ -36,7 +40,7 @@ class Menu extends Component {
         const menu = this.props.dishes.map((dish) => {
             return (
               <div key={dish.id} className="col-12 col-md-5 m-1">
-                <Card onClick= {() => this.onDishSelect(dish)} >
+                <Card onClick= {() => this.props.onClick(dish.id)} >
                     <CardImg width="100%" src={dish.image} alt={dish.name} />
                   <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
@@ -46,18 +50,16 @@ class Menu extends Component {
             );
         });
 
+        console.log('Menu Component Render is invoked');
+
         return (
-          <div className="container">
-            <div className="row">
-                  {menu}
+            <div className="container">
+                 <div className="row">
+                      {menu}
+                </div>
+                <div className="row">
+                </div>
             </div>
-
-            <div className= "row" >
-                {this.renderDish(this.state.selectedDish)}
-            </div>
-
-
-          </div>
         );
     }
 }
