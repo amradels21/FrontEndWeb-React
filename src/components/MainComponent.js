e17e935a9755cc1641490dfd1e9b37c9e3d39844
Amr Adel <amradel21@icloud.com>
2020-06-04 00:28:27 +0200

    AboutUs Component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -13,6 +13,7 @@ import { PROMOTIONS } from '../shared/promotions';
 
 
 import { Switch, Route, Redirect } from 'react-router-dom';
+import About from './AboutComponent';
 
 class Main extends Component {
 
@@ -59,6 +60,7 @@ class Main extends Component {
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route exact path='/contactus' component={Contact} />} />
+              <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
               <Redirect to="/home" />
           </Switch>
